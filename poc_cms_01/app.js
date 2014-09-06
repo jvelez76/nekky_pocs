c6807066811827afcbf1a28380850e3cd791b425
jvelez76 <Vmimz.76.2014>
2014-09-06 13:09:25 -0500

    222

--- poc_cms_01/app.js
+++ poc_cms_01/app.js
@@ -1,3 +1,15 @@
+/**
+ * NEKKY GROUP, S.A. DE C.V.
+ * -------------------------------------
+ * CMS POC.
+ * -------------------------------------
+ * Javier Velez <javierv@nekky.org>
+ * -------------------------------------
+ * app.js
+ * Last modification: 09/05/2014
+ * -------------------------------------
+ * @type {exports}
+ */
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -10,12 +22,15 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+/**
+ * Set views
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+/* uncomment after placing your favicon in /public
+   app.use(favicon(__dirname + '/public/favicon.ico'));
+*/
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,17 +40,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+/**
+ * catch 404 and forward to error handler
+ */
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
+/**
+ * Error handlers
+ */
 
-// development error handler
-// will print stacktrace
+/**
+ * Development error handler will print stacktrace
+ */
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -46,8 +66,6 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
@@ -56,5 +74,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
 module.exports = app;
+
